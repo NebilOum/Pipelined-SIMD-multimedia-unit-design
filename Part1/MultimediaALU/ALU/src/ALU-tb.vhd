266f23f5fe21f903dbc86d9b1374837a5f691b12
NebilOum <29616059+NebilOum@users.noreply.github.com>
2022-10-25 10:57:29 -0400

    Changed mult, add, and sub architectures

--- Part1/MultimediaALU/ALU/src/ALU-tb.vhd
+++ Part1/MultimediaALU/ALU/src/ALU-tb.vhd
@@ -8,17 +8,27 @@ use work.all;
 entity alu_tb is
 end alu_tb;
 
-architecture testbench of alu_tb is
-	port map(
-		clk => clk
-		inReg1 => ir1
-		inReg2 => ir2
-		inReg3 => ir3,	
-		intsReg3 => iR3,
-		intsReg4 => iR4,
-		outReg => oReg
-		); 
+architecture testbench of alu_tb is	  
+	signal clk: std_logic;
+	signal ir1,ir2,ir3 : std_logic_vector(127 downto 0); 
+	signal insR3,insR4 : std_logic_vector(24 downto 0);
+	signal oReg : std_logic_vector(127 downto 0); 
 --signal;
-begin 
+begin  
+	uut:entity aluIO port map(
+		clk => clk,
+		inReg1 => ir1,
+		inReg2 => ir2,
+		inReg3 => ir3,	
+		insReg3 => insR3,
+		insReg4 => insR4,
+		outReg => oReg
+		);		
+	process
+	begin
+--		for i in 0 to loop
+--			assert oReg  report "failed" severtiy failure;
+--		end loop;
+	end process;
 	
 end testbench;
--- Part1/MultimediaALU/ALU/src/ALU.vhd
+++ Part1/MultimediaALU/ALU/src/ALU.vhd
@@ -1,63 +1,128 @@
+----------------------------------------------------------------------------------------
+---ESE 345 Porject: Pipelined SIMD multimedia unit design - Part 1	
+---Authors: Nebil Oumer and Elijah Farrell
+---File:ALU.vhdl
+---10/30/2022 
+----------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------multiplication of n bits 
 library ieee;
-use ieee.std_logic_1164.all;
-use work.all;
+use ieee.std_logic_1164.all; 
+use ieee.numeric_std.all;
+use work.all; 
 
-----------------------------------------------------------------------------------------multiplication of n bits
 entity multiplication	is 
-	generic (m : integer);
+	generic (m : integer := 16); --- generic used so we can do operation on multiple bit lengths
 	port(
+	clk, xsign : in std_logic;---- xsign used to do either unsigned or signed operations
 	input1,input2 :in std_logic_vector(m-1 downto 0); --input values
-	output: in std_logic_vector(m-1 downto 0); --- output values
+	output: out std_logic_vector(m-1 downto 0) --- output values
 	);
 end multiplication; 
 
-architecture multAlu of multiplication is 
+architecture multAlu of multiplication is 	   
+ 	----- procedure to do multiplication of two n-bit inputs
+	procedure mult(signal in1,in2 : in std_logic_vector(m-1 downto 0);
+				   signal sign : in std_logic;
+				   signal oput : out std_logic_vector(m-1 downto 0)) is
+	begin
+		if xsign = '0'then 	--- unsigned multiplicatin
+			oput <= std_logic_vector(resize(unsigned(in1) * unsigned(in2),m));
+		else 				--- signed multiplication
+			oput <= std_logic_vector(resize((signed(in1) * signed(in2)),m)); 
+		end if;
+	end procedure;
 begin 
-	---- when else to do either a unsigned or signed operation
-	output <= std_logic_vector(resize(unsigned(input1) * unsigned(input2),m)) when xsign = '0' else std_logic_vector(resize(input1 * input2,a));
-end architecture multAlu;
-----------------------------------------------------------------------------------------
+	process(input1,input2)
+	begin
+		if rising_edge(clk)	then
+			 mult(input1,input2,xsign,output);
+		end if;
+	end process;
+end architecture multAlu;---- behavioral archtitecure for multiplication
+---------------------------------------------------------------------------------------- 
+----------------------------------------------------------------------------------------addition of n bits	  
+library ieee;
+use ieee.std_logic_1164.all; 
+use ieee.numeric_std.all;
+use work.all; 
 
-----------------------------------------------------------------------------------------addition of n bits
 entity addition	is 
-	generic (a : integer);
-	port(								
-	xsign : in std_logic;
-	input1,input2 :in std_logic_vector(a-1 downto 0); --input values
-	output: in std_logic_vector(a-1 downto 0); --- output values
+	generic (a : integer := 16); --- generic used so we can do operation on multiple bit lengths
+	port(
+	clk, xsign : in std_logic;---- xsign used to do either unsigned or signed operations
+	input1,input2 :in std_logic_vector(m-1 downto 0); --input values
+	output: out std_logic_vector(m-1 downto 0) --- output values
 	);
 end addition; 
 
-architecture addAlu of addition is 
+architecture addAlu of addition is
+	---- prodcedure to do  adddition of two n bit inputs
+	procedure add(signal in1,in2 : in std_logic_vector(a-1 downto 0);
+				   signal sign : in std_logic;
+				   signal oput : out std_logic_vector(a-1 downto 0)) is
+	begin
+		if xsign = '0'then 	  ---- unsigned addition
+			oput <= std_logic_vector(resize(unsigned(in1) + unsigned(in2),a));
+		else 				  ---- signed addition
+			oput <= std_logic_vector(resize((signed(in1) + signed(in2)),a)); 
+		end if;
+	end procedure;
 begin 
-	---- when else to do either a unsigned or signed operation
-   output <= std_logic_vector(resize(unsigned(input1) + unsigned(input2),a)) when xsign = '0' else std_logic_vector(resize(input1 + input2,a));
-end architecture addAlu;
+	process(input1,input2)
+	begin
+		if rising_edge(clk)	then
+			 add(input1,input2,xsign,output);
+		end if;
+	end process;
+end architecture addAlu;	--- behavioral archtitecure for addition
 -----------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------subtraction of n bits	
+library ieee;
+use ieee.std_logic_1164.all; 
+use ieee.numeric_std.all;
+use work.all; 
 
------------------------------------------------------------------------------------------subtraction of n bits
 entity subtraction	is 
-	generic (s : integer);
+	generic (s : integer := 16);
 	port(
+	clk, xsign : in std_logic;
 	input1,input2 :in std_logic_vector(s-1 downto 0); --input values
-	output: in std_logic_vector(s-1 downto 0); --- output values
+	output: out std_logic_vector(s-1 downto 0) --- output values
 	);
 end subtraction; 
 
 architecture subAlu of subtraction is 
-begin	
-   ---- when else to do either a unsigned or signed operation
-   output <= std_logic_vector(resize(unsigned(input1) - unsigned(input2),s)) when xsign = '0' else std_logic_vector(resize(input1 - input2,a));
-end architecture subAlu;
+	---- prodcedure to do  subtraction of two n bit inputs
+	procedure sub(signal in1,in2 : in std_logic_vector(s-1 downto 0);
+				   signal sign : in std_logic;
+				   signal oput : out std_logic_vector(s-1 downto 0)) is
+	begin
+		if xsign = '0'then 	  --- unsigned subtraction
+			oput <= std_logic_vector(resize(unsigned(in1) - unsigned(in2),s));
+		else 				  --- signed subtraction
+			oput <= std_logic_vector(resize((signed(in1) - signed(in2)),s)); 
+		end if;
+	end procedure;
+begin 
+	process(clk)
+	begin
+		if rising_edge(clk)	then
+			 sub(input1,input2,xsign,output);
+		end if;
+	end process;
+end architecture subAlu; --- behavioral archtitecure for subtraction
 -----------------------------------------------------------------------------------------
-
+library ieee;								  	`
+use ieee.std_logic_1164.all; 
+use ieee.numeric_std.all;
+use work.all;  
 
 entity aluIO is   --- the three 128 bit inputs and the one 128 bit output register
 	port(
 	clk: in std_logic;
-	inReg1, inReg2, inReg3: in std_logic_vector(127 downto 0);	
-	intsReg3,intsReg4 : in std_logic_vector(24 downto 0); 
-	outReg: out std_logic_vector(127 downto 0);
+	inReg1, inReg2, inReg3: in std_logic_vector(127 downto 0);	---- input registers
+	insReg3,insReg4 : in std_logic_vector(24 downto 0); ---- instruction input
+	outReg: out std_logic_vector(127 downto 0)	  ---- output register
 	);
 end aluIO;
 
